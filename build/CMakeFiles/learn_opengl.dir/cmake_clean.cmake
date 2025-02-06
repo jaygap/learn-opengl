cbf52a9ab121d782fa807a1cf91ca2b01f46ef14
joseg <j.apariciopinto@example.com>
2025-02-06 16:25:33 +0000

    starting (glfw working)

---
+++ build/CMakeFiles/learn_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_opengl.dir/link.d"
+  "CMakeFiles/learn_opengl.dir/src/main.cpp.o"
+  "CMakeFiles/learn_opengl.dir/src/main.cpp.o.d"
+  "learn_opengl"
+  "learn_opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/learn_opengl.dir/link.d
@@ -0,0 +1,28 @@
+learn_opengl: \
+  /usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/14/libgcc_s.so:
+
+/lib64/ld-linux-x86-64.so.2:
